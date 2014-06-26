4aec784158dec71740deaac916b8fbe4a1771f8b
Nikita.Kuhta <nikita.kuhta@integral.obninsk.ru>
2014-06-26 18:13:25 +0400

    Picker for references added

---
+++ src/coffee/Cti/Picker.coffee
@@ -0,0 +1,37 @@
+Ext.define 'Cti.Picker',
+  xtype: 'ctipicker'
+  extend: 'Ext.form.field.ComboBox'
+  queryMode: 'local'
+  minChars: 0
+  enableKeyEvents: true
+  directFn: Storage.getList
+  anyMatch: true
+  errorMessage: 'Value not found'
+  initComponent: ->
+    window.pik = this
+    @store =
+      autoDestroy: true
+      fields: [@valueField, @displayField]
+      proxy: 'memory'
+    @callParent arguments
+    @on 'afterrender', =>
+      @load()
+
+  setLoadingState: (key) ->
+    if key
+      @getPicker().setHeight 100
+      @getPicker().setLoading true
+    else
+      @getPicker().setLoading false
+
+  load: ->
+    @setLoadingState true
+    @directFn @model, (records) =>
+      @store.loadData records.data
+
+  validator: (value) ->
+    return true if value is "" and @allowBlank is true
+    existingRecord = this.findRecord @valueField, @getValue()
+    return @errorMessage unless existingRecord
+    true
+
--- src/php/Generator/Form.php
+++ src/php/Generator/Form.php
@@ -6,6 +6,11 @@ use Cti\Core\String;
 
 class Form extends Generator
 {
+    /**
+     * @var \Cti\Storage\Component\Model
+     */
+    public $model;
+
     public function getGeneratedCode()
     {
         $class = $this->model->getClassName();
@@ -16,24 +21,47 @@ class Form extends Generator
         $idProperty = json_encode(count($pk) == 1 ? $pk[0] : $pk);
 
         $items = array();
-        foreach($this->model->getProperties() as $property) {
 
+        // @todo References only by 1 field
+        /**
+         * @var \Cti\Storage\Component\Reference[] $referenceByField
+         */
+        $referenceByField = array();
+        foreach($this->model->getOutReferences() as $reference) {
+            if (count($reference->getProperties()) == 1) {
+                $properties = $reference->getProperties();
+                $property = array_shift($properties);
+                $referenceByField[$property->getName()] = $reference;
+            }
+        }
+
+        foreach($this->model->getProperties() as $property) {
+            $reference = isset($referenceByField[$property->getName()]) ?
+                $referenceByField[$property->getName()] :
+                null;
             $item = array(
                 'name' => $property->getName(), 
                 'allowBlank' => !!$property->getRequired(),
                 'fieldLabel' => $property->getComment(),
             );
 
-            switch($property->getJavascriptType()) {
-                case 'date': 
-                    $item['xtype'] = 'datefield';
-                    break;
-                case 'numeric': 
-                    $item['xtype'] = 'numberfield';
-                    break;
-                default:
-                    $item['xtype'] = 'textfield';
-                    break;
+            if ($reference) {
+                $item['xtype'] = 'ctipicker';
+                $item['model'] = $reference->getDestination();
+                $item['displayField'] = 'name';
+                $item['valueField'] = 'id_' . $reference->getDestination();
+            } else {
+                switch($property->getJavascriptType()) {
+                    case 'date':
+                        $item['xtype'] = 'datefield';
+                        break;
+                    case 'numeric':
+                        $item['xtype'] = 'numberfield';
+                        break;
+                    default:
+                        $item['xtype'] = 'textfield';
+                        break;
+                }
             }
 
             if($property->getBehaviour()) {
@@ -59,6 +87,7 @@ class Form extends Generator
         return <<<COFFEE
 Ext.define 'Generated.Form.$class',
 
+  requires: ['Cti.Picker'],
   extend: 'Ext.form.Panel'
   bodyPadding: 10
   monitorValid: true
@@ -66,7 +95,7 @@ Ext.define 'Generated.Form.$class',
 
   getPk: -> $pk_getter
 
-  items: $items
+  getItemsConfig: -> $items
 
   getBottomToolbar: ->
     [
@@ -83,6 +112,7 @@ Ext.define 'Generated.Form.$class',
 
   initComponent: ->
     @bbar = @getBottomToolbar()
+    @items = @getItemsConfig()
     @callParent arguments
 
     if @modelExists()
